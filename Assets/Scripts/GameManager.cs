5265f73e614d1c7bd4d050bafb9e38c843597f0c
Osinko <yoshinari_m@msn.com>
2014-07-22 01:08:45 +0900

    no message

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+		void Start ()
+		{
+				BeginGame ();
+		}
+	
+		void Update ()
+		{
+				if (Input.GetKeyDown (KeyCode.Space)) {
+						RestartGame ();
+				}
+		}
+
+		public Maze mazePrefab;
+		Maze mazeInstance;
+
+		void BeginGame ()
+		{
+				mazeInstance = Instantiate (mazePrefab) as Maze;
+		}
+
+		void RestartGame ()
+		{
+				Destroy (mazeInstance);
+				BeginGame ();
+		}
+}
