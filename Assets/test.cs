05d01e983d97e9956cb3ababa23e936dfafa6fe6
Osinko <yoshinari_m@msn.com>
2014-07-28 01:04:51 +0900

    no message

---
+++ Assets/test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
